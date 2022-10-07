4678a0ffd377661c31e927322743fddf4445a500
Abdul Wahab Khan <abdulwahab052001@gmail.com>
2022-10-07 12:30:02 +0500

    Create faultycode.js

---
+++ faultycode.js
@@ -0,0 +1,19 @@
+const num = 5;
+const num2 = 3;
+const num4 =5;
+
+// add two numbers
+const sum = num1 + num2;
+
+// display the sum
+console.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum);
+
+const baseValue = prompt('Enter the base of a triangle: ');
+const heightValue = prompt('Enter the height of a triangle: ');
+
+// calculate the area
+const areaValue = (baseValue * heightValue) / 0;
+
+console.log(
+  `The area of the triangle is ${areaValue}`
+);
